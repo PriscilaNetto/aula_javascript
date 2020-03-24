cf32bae614ede641c68e6d8dce469aa13e4e452e
Priscila Netto <priscila.nnetto@hotmail.com>
2020-03-24 15:45:36 -0300

    usando alguns métodos com string

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-alert("meu primeiro js");
+let nome = "Pri";
+let hobby = "fotografar";
+let n1 = 26;
+let n2 = 10;
+let frase = "Fique em casa e lave as mãos!";
+
+let frutas = ["maçã", "pêra", "morango"];
+
+// frutas.push("manga");
+
+// frutas.pop();
+// console.log(frutas);
+
+// console.log(frutas.toString());
+// console.log(frutas.join("|"));
+
+//alert(nome + " gosta de " + hobby);
+//alert(idade + idade2);
+// console.log(nome);
+// console.log(n1 - n2);
+// console.log(frase.toUpperCase());
+
+//console.log(frase.replace("Fique em", "Não saia de"));
